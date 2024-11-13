da1ebff87b186d14f8a2e0b2b616e2b9e6720fce
ychv <ychv888@gmail.com>
2024-11-13 16:59:04 -0500

    Started herons formula, still an error with output

--- src/components/HeronsFormula.jsx
+++ src/components/HeronsFormula.jsx
@@ -3,14 +3,14 @@ import { useState } from 'react';
 
 function HeronsFormula() {
 
-    const [a, setA] = useState(0);
-    const [b, setB] = useState(0);
-    const [c, setC] = useState(0);
-    const [area, setArea] = useState(0);
+    const [a, setA] = useState('');
+    const [b, setB] = useState('');
+    const [c, setC] = useState('');
+    const [area, setArea] = useState('');
 
     function heronsFormula(e) {
         e.preventDefault();
-        setAns((1 / 4) * Math.sqrt(4 * a ** 2 * b ** 2 - (a ** 2 + b ** 2 - c ** 2) ** 2));
+        setArea((1 / 4) * Math.sqrt(4 * a ** 2 * b ** 2 - (a ** 2 + b ** 2 - c ** 2) ** 2));
     }
 
     return (
@@ -21,10 +21,10 @@ function HeronsFormula() {
             <label>Side B:</label>
             <input type="number" value={b} onChange={(event) => { setB(event.target.value) }} required />
             <label>Side C:</label>
-            <input type="number" value={b} onChange={(event) => { setC(event.target.value) }} required />
+            <input type="number" value={c} onChange={(event) => { setC(event.target.value) }} required />
             <label>Area (Result):</label>
             <input type="text" value={area} readOnly />
-            <button type="submit" value="Calculate"></button>
+            <button type="submit">Calculate</button>
         </form>
     )
 
