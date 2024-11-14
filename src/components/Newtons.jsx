05a3efa7ffa84048cde78c2e7eb3b3510bc72300
chy7u <yanjincchen@gmail.com>
2024-11-14 12:21:36 -0500

    Started newtons method.

---
+++ src/components/Newtons.jsx
@@ -0,0 +1,16 @@
+import './Newtons.css';
+import { useState } from 'react';
+
+function NewtonsMethod() {
+    
+    const [func, setF] = useState('');
+    const [derivative, setD] = useState(''); 
+    const [ans, setAns] = useState('');
+
+    function newtons(e) {
+        e.preventDefault();
+
+        
+    }
+
+}
